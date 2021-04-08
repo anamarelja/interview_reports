b426cdcb61acc0e5d6059eb7162369db5b8a45be
BIT Student <japax97@gmail.com>
2021-04-08 16:42:04 +0200

    Fixed issues

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,26 +1,21 @@
-
-import React, { useState,useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./user.scss";
 import Login from "../../components/Login";
 import Report from "../../components/Report";
-import {candidateContext} from "../../App";   
+import { candidateContext } from "../../App";
 
 import { Link } from "react-router-dom";
 import { computeHeadingLevel } from "@testing-library/dom";
 
 const User = (props) => {
-
   const [reports, setReports] = useState([]);
 
   const { id: matchId } = props.match.params;
   console.log(matchId);
 
-
   const candidates = useContext(candidateContext);
 
-const user = candidates.find ((e) => e.id==props.match.params.id);
-
-
+  const user = candidates.find((e) => e.id == props.match.params.id);
 
   useEffect(() => {
     fetch("http://localhost:3333/api/reports")
@@ -28,7 +23,7 @@ const user = candidates.find ((e) => e.id==props.match.params.id);
       .then((reports) => setReports(reports));
   }, []);
 
-  if(candidates.length){
+  if (candidates.length) {
     return (
       <div className="User">
         <header>
@@ -37,26 +32,39 @@ const user = candidates.find ((e) => e.id==props.match.params.id);
           </Link>
           <Login />
         </header>
-  
+
         <div className="user-info">
-          
           <img src={user.avatar} alt="pic" />
-  
+
           <div className="user-desc">
             <h3>Name: {user.name}</h3>
             <h4>Education: {user.education}</h4>
             <h4>Email: {user.email}</h4>
           </div>
+        </div>
 
-
+        <div className="report-list">
+          <div className="report-header">
+            <span>
+              <h4>Company</h4>
+            </span>
+            <span>
+              <h4>Interview Date</h4>
+            </span>
+            <span>
+              <h4>Status</h4>
+            </span>
+          </div>
+        </div>
+        
+        {reports
+          .filter((report) => matchId == report.candidateId)
+          .map((e) => (
+            <Report reportInfo={e} />
+          ))}
       </div>
-      {reports.filter(report => matchId == report.candidateId).map(e =>(
-        <Report reportInfo={e}/>
-        ))}
-
-    </div>
-  );
-}
+    );
+  }
 };
 
 export default User;
