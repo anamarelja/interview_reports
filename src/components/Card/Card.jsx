08140623ad49d1565cf3384a71b2a65d25cd3d41
vukanvu <80677866+vukanvu@users.noreply.github.com>
2021-04-08 13:40:23 +0200

    Vukan branch (#4)
    * uradio home.page, fatch u App.js,link za user.page
    
    * pera

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-import './style.scss'
+import React, { useContext, useState } from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
 
-const Card = () => {
-    return ( 
-        <div className="Card">
-            Card
-        </div>
-     );
-}
- 
+const Card = (props) => {
+  return (
+    <Link to={`/user${props.user.id}`}>
+      <div className="Card">
+        <img src={props.user.avatar} alt="user-image" />
+        <h3>{props.user.name}</h3>
+        <h4>{props.user.email}</h4>
+      </div>
+    </Link>
+  );
+};
+
 export default Card;
