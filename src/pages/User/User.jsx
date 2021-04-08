4465537a80cfc7c30cc3772ba5a33bdc2f557bac
Ana Marelja <anamarelja@Anas-MacBook-Pro.local>
2021-04-08 16:51:44 +0200

    Changing user

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -11,12 +11,8 @@ const User = (props) => {
   const [reports, setReports] = useState([]);
 
   const { id: matchId } = props.match.params;
-  console.log(matchId);
-
   const candidates = useContext(candidateContext);
-
   const user = candidates.find((e) => e.id == props.match.params.id);
-
 
   useEffect(() => {
     fetch("http://localhost:3333/api/reports")
@@ -36,7 +32,6 @@ const User = (props) => {
 
         <div className="user-info">
           <img src={user.avatar} alt="pic" />
-
           <div className="user-desc">
             <h3>Name: {user.name}</h3>
             <h4>Education: {user.education}</h4>
@@ -57,7 +52,7 @@ const User = (props) => {
             </span>
           </div>
         </div>
-        
+
         {reports
           .filter((report) => matchId == report.candidateId)
           .map((e) => (
