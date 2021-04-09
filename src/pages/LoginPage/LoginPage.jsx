6331ef4a66332630f7807cfd95819b567c62ccaf
anamarelja <80677680+anamarelja@users.noreply.github.com>
2021-04-09 14:07:38 +0200

    login page finished (#8)
    Co-authored-by: Ana Marelja <anamarelja@Anas-MacBook-Pro.local>

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,27 +1,68 @@
-import React, { useState } from 'react';
-import {  Link } from 'react-router-dom'
-import './style.scss'
+import React, { useState, useEffect, useContext } from "react";
+import { Link, Redirect } from "react-router-dom";
+import "./style.scss";
+import { tokenContext } from "../../App";
+import OurFetch from "../../OurFetch";
 
 const LoginPage = () => {
-    return ( 
-        <div className="LoginPage">
-            <Link to='/'>  <button className="btn-home">BACK TO HOME</button></Link>
-          
-            <div className="Form"> 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { token, setToken } = useContext(tokenContext);
+  const [error, setError] = useState("");
 
-                <label for="usernmae">Username: </label>
-                <input type="text" name="username"/>
+  const onUserLogin = () => {
+    OurFetch("http://localhost:3333/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: username,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => {
+        setError(false);
+        setToken(data);
+      })
+      .catch((g) => {
+        setError(true);
+      });
+  };
 
-                <label for="password">Password: </label>
-                <input type="text" name="password"/>
+  return (
+    <div className={error ? "errorPage" : "LoginPage"}>
+      <Link to="/">
+        {" "}
+        <button className="btn-home">BACK TO HOME</button>
+      </Link>
 
+      <div className="Form">
+        <label for="usernmae">Username: </label>
+        <input
+          type="text"
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
 
-               <button>LoginPage</button>
-                
-                
-            </div>
-        </div>
-     );
-}
- 
+        <label for="password">Password: </label>
+        <input
+          type="text"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+
+        <button onClick={onUserLogin}>LoginPage</button>
+        {token && <Redirect to="/admin" />}
+
+        {error ? (
+          <p>Incorrect username or password. Please try again.</p>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
 export default LoginPage;
